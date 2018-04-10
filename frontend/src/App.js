e7e13e17d85d9f2d6017ded3ea43f23c6a16e142
stPhoenix <bohdansapovsky@gmail.com>
2018-04-10 22:39:52 +0300

    new updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,17 @@ import './App.css';
 import axios from 'axios';
 
 class App extends Component {
-  doSmth () {
-     axios.get("http://127.0.0.1:8000/articles/")
+    constructor(props){
+        super(props);
+        this.state = {
+            data: 'Initial data',
+        };
+        this.doSmth = this.doSmth.bind(this);
+    }
+  doSmth (e, link) {
+     axios.get(`http://127.0.0.1:8000/${link}/`)
         .then (res => {
-         document.getElementById('result').innerHTML = res.data[0].title;
+         this.setState({data:JSON.stringify(res.data)});
      })
         .catch(e => {
          console.log(e)
@@ -24,8 +31,13 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <div id="result">
-            <button value="Click me" onClick={this.doSmth}> click me </button>
+            <p><button value="Articles" onClick={e => this.doSmth(e, 'articles')}> Articles </button>
+            <button value="Articles" onClick={e => this.doSmth(e, 'articles/2')}> Article 2 </button>
+            <button value="Articles" onClick={e => this.doSmth(e, 'users')}> Users </button>
+            <button value="Articles" onClick={e => this.doSmth(e, 'users/1')}> User 1 </button></p>
             <p> <a href="http://127.0.0.1:8000/articles/"> LINK </a> </p>
+        </div>
+        <div>{this.state.data}
         </div>
       </div>
     );
